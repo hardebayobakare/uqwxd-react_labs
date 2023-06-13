141a8ea5ce52aeef6f9baa7a2296110d5347199f
hardebayo <hardebayyobakare@gmail.com>
2023-06-13 11:09:20 -0400

    Added ToDo Features

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,27 +3,109 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
+  const [todoEditing, setTodoEditing] = React.useState(null);
+  const [editingText, setEditingText] = React.useState("");
+
+  React.useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+  React.useEffect(() => {
+    if (todos.length > 0) {
+        const json = JSON.stringify(todos);
+        localStorage.setItem("todos", json);
+    }
+  }, [todos]);
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+      e.preventDefault();
+
+      const newTodo = {
+          id: new Date().getTime(),
+          text: todo.trim(),
+          completed: false,
+      };
+      if (newTodo.text.length > 0) {
+          setTodos([...todos].concat(newTodo));
+          setTodo("");
+      } else {
+          alert("Enter Valid  Task");
+          setTodo("");
+      }
+  }
   
   // Add the deleteToDo code here
-
+  function deleteToDo(id) {
+      if (window.confirm("Are you sure you want to delete the task?") === true){
+        let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+        alert("To do deleted successfully");
+      }
+    }
   
   // Add the toggleComplete code here
+  function toggleComplete(id){
+      let updatedTodo =[...todos].map((todo) => {
+          if (todo.id === id) {
+              todo.completed = !todo.completed;
+          }
+          return todo;
+      });
+      setTodos(updatedTodo);
+  }
 
   
   // Add the submitEdits code here
+  function submitEdits(id){
+    let updatedTodos = [...todos].map((todo) => {
+        if (todo.id === id){
+            if (editingText.length > 0) {
+                todo.text = editingText;
+            } else {
+                alert("Enter Valid  Task");
+            }
+            
+        }
+        return todo; 
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+  }
 
-  
+
 return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+    <div className ="App">
+        <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+            <input type ="text" align ="right" onChange={(e) => setTodo(e.target.value)} placeholder="Add a new Task" value={todo}/>
+            <button type ="submit">Add Todo</button>
+        </form>
+        {todos.map((todo) => (
+            <div key={todo.id} className="todo">
+              <div className="todo-text">
+                <input type="checkbox" id="completed" checked={todo.completed} onChange={() => toggleComplete(todo.id)}/>
+                {todo.id === todoEditing ? (
+                  <input type="text" onChange={(e) => setEditingText(e.target.value)}/>
+                ) : (
+                  <div>{todo.text}</div>
+                )}
+              </div>
+              <div className="todo-actions">
+                {todo.id === todoEditing ? (
+                  <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+                ) : (
+                  <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+                )}
+                <button onClick={() => deleteToDo(todo.id)}>Delete</button>
+              </div>
+            </div>
+          ))}
+    </div>
+    );
+
 };
 export default App;
